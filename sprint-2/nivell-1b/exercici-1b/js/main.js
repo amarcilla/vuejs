6427357b615654f2b3d838aca753c94760e37616
amarcilla <lamarcilla@gmail.com>
2021-11-10 13:13:52 +0100

    tabs + faqs

--- sprint-2/nivell-1b/exercici-1b/js/main.js
+++ sprint-2/nivell-1b/exercici-1b/js/main.js
@@ -30,3 +30,13 @@ $(function() {
 
 	
 })
+
+//Acordios. FAQS
+$(".accordion_tab").click(function(){
+    $(".accordion_tab").each(function(){
+      $(this).parent().removeClass("active");
+      $(this).removeClass("active");
+    });
+    $(this).parent().addClass("active");
+    $(this).addClass("active");
+});
