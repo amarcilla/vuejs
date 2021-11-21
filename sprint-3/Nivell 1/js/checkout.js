a435a18a6e19fa6061e8287e805d03798f38b3be
amarcilla <annaestic@gmail.com>
2021-11-21 09:09:08 +0100

    comencem amb forms

--- sprint-3/Nivell 1/js/checkout.js
+++ sprint-3/Nivell 1/js/checkout.js
@@ -1,7 +1,13 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+// var password = document.querySelector(".password");
+// var phone = document.querySelector('.Phone No.');
+// var name = document.querySelector('.First Name');
+// var name = document.querySelector('.Last Name');
+// var name = document.querySelector('.Email');
+// var name = document.querySelector('.Address');
+var nom = document.querySelector('.firstName');
+
+
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
@@ -11,4 +17,6 @@ var errorPhone = document.getElementById('errorPhone');
 // Exercise 8
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+    console.log(nom.value);
+
 }
