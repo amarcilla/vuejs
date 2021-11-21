03eede91a61aa3358cf5602f5b842a2b87d9b16c
amarcilla <annaestic@gmail.com>
2021-11-21 11:13:33 +0100

    Ex 8 froms

--- sprint-3/Nivell 1/js/checkout.js
+++ sprint-3/Nivell 1/js/checkout.js
@@ -5,7 +5,12 @@
 // var name = document.querySelector('.Last Name');
 // var name = document.querySelector('.Email');
 // var name = document.querySelector('.Address');
-var nom = document.querySelector('.firstName');
+var nom = document.querySelector('#firstName');
+var cognom = document.querySelector('#lastName');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+var adresa= document.querySelector('#address');
+var telefon = document.querySelector('#phone');
 
 
 
@@ -17,6 +22,13 @@ var errorPhone = document.getElementById('errorPhone');
 // Exercise 8
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+
+    if (nom.value === "" || cognom.value === "") {
+        alert("All fields are required!");
+     }
+
     console.log(nom.value);
+    nom.classList.add("error");
+
 
 }
